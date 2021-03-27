8bdfb34b33b9b41e1c732aa4ff3ef692913b802a
Ibrahim Banat <ibrahim.banat.97@gmail.com>
2021-03-27 20:13:07 +0300

    adding functionality to the form

--- js/app.js
+++ js/app.js
@@ -1,1 +1,86 @@
 'use strict';
+
+// constructor
+
+const Note = function (title, noteText, isPinned, background) {
+  this.title = title;
+  this.noteText = noteText;
+  this.isPinned = isPinned;
+  this.background = background;
+  Note.all.push(this);
+};
+Note.all = [];
+Note.prototype.addToLocalStorage = function () {
+  localStorage.setItem('Notes', JSON.stringify(Note.all));
+};
+
+if (localStorage.getItem('Notes')) {
+  Note.all = JSON.parse(localStorage.getItem('Notes'));
+}
+
+$('#form').on('submit', function (event) {
+  event.preventDefault();
+  console.log(event.target);
+  let title = $('#title').val();
+  let text = $('#note').val();
+  let pin = $('#pin').attr('clicked');
+  let background = $('.inpt--ctr').css('background-color');
+  console.log(title, text, pin, background);
+  $('#form').trigger('reset');
+  let note = new Note(title, text, pin, background);
+  console.log(Note.all);
+  note.addToLocalStorage();
+});
+
+$('#pin').click(function () {
+  console.log('clicked');
+  if ($('#pin').attr('clicked') === 'false') {
+    $('#pin').attr('name', 'pin');
+    $('#pin').attr('clicked', 'true');
+  } else {
+    $('#pin').attr('name', 'pin-outline');
+    $('#pin').attr('clicked', 'false');
+  }
+});
+
+$('#trash').click(function () {
+  $('#form').trigger('reset');
+  $('#pin').attr('name', 'pin-outline');
+  $('.inpt--ctr').css('background-color', '#dddddc');
+  $('#labels').html('');
+});
+
+$('.span1, .span2, .span3, .span4').click(function (event) {
+  // console.log($(`${event.target.}`).css('background-color'));
+
+  let cardBackground = $(this).css('background-color');
+  $('.inpt--ctr').css('background-color', cardBackground);
+});
+$('.label').on('click', function () {
+  let label = $(this).children('.labelInner').text();
+  $('#labels').append(
+    `<div class="added"><div class="added--text">${label}</div><span class="remove">X</span></div>`
+  );
+});
+
+$('#inputLabel').keypress(function (event) {
+  var keycode = event.keyCode ? event.keyCode : event.which;
+  if (keycode == '13') {
+    let content = $(this).val();
+    $(`<div class="label1">
+    <ion-icon name="ticket-outline"></ion-icon>
+    <span class="labelInner">${content}</span></div>`).insertAfter(this);
+    $(this).val('');
+    $('.label1').on('click', function () {
+      let label = $(this).children('.labelInner').text();
+      $('#labels').append(
+        `<div class="added"><div class="added--text">${label}</div><span class="remove">X</span></div>`
+      );
+    });
+  }
+});
+
+$('.remove').on('click', function () {
+  console.log('ayooooooooooooo');
+  $(this).parent().remove();
+});
